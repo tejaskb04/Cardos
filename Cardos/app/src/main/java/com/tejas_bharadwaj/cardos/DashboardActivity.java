9b78f90bee59c165b71329a1665c83a53aca9d1a
tejaskb04 <tejaskb@gmail.com>
2017-10-06 11:20:57 -0700

    Map Code

--- Cardos/app/src/main/java/com/tejas_bharadwaj/cardos/DashboardActivity.java
+++ Cardos/app/src/main/java/com/tejas_bharadwaj/cardos/DashboardActivity.java
@@ -1,13 +1,29 @@
 package com.tejas_bharadwaj.cardos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class DashboardActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class DashboardActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private final String GOOGLE_PLACES_API_KEY = "AIzaSyAH008n41rXGsO2oYtJgZduebNYwN127_I";
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(DashboardActivity.this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.getUiSettings().setMapToolbarEnabled(false);
     }
 }
