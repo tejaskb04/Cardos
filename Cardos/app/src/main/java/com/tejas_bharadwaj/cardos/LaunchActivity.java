05e3d83033a77a64036584b4e775e58adb027ba2
tejaskb04 <tejaskb@gmail.com>
2017-10-06 11:08:39 -0700

    Setup

--- Cardos/app/src/main/java/com/tejas_bharadwaj/cardos/LaunchActivity.java
+++ Cardos/app/src/main/java/com/tejas_bharadwaj/cardos/LaunchActivity.java
@@ -1,13 +1,19 @@
 package com.tejas_bharadwaj.cardos;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class LaunchActivity extends AppCompatActivity {
+
+    private LinearLayout launchLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        launchLayout = (LinearLayout) findViewById(R.id.launch_layout);
+        launchLayout.setBackgroundColor(Color.parseColor("#194D1E"));
     }
 }
